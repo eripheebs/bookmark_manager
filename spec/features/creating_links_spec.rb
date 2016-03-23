f1bd9aebf505244b5ca491637d67345539f482bf
eripheebs <erikapheby@gmail.com>
2016-03-23 10:58:23 +0000

    Adding new link function passing feature test

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,14 @@
+feature '#Creating links' do
+  scenario '> should add link to database' do
+    visit ('/links/new')
+    fill_in :url, with: 'www.google.com'
+    fill_in :title, with: 'Google'
+    click_button 'Create Link'
+
+    expect(current_path).to eq '/links'
+
+    within 'ul#links' do
+      expect(page).to have_content('Google')
+    end
+  end
+end
